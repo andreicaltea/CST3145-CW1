542fd3acb0ee3235e1e789ac7e94928c761ca2e4
andreicaltea <andreicaltea@yahoo.com>
2022-05-09 00:03:36 +0100

    added .js file, images folder and a new div

---
+++ main.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'LESSONS LIST',
+            description: 'Please use "ADD TO CART" buttton to book a lesson!',
+
+        }
+    }
+})
+
+const app2 = Vue.createApp({
+    data() {
+        return {
+            product1: 'ENGLISH LESSON',
+            description: 'Learn how to speak English',
+            location: 'Wembley, London',
+            price: '£150.00',
+            spaces: '5',
+
+        }
+    }
+})
